42ab6a3e68bf8754a2e8e06f03e67d42ff790142
Ryan Floyd <ryanfloyd@live.com>
2015-10-25 22:36:06 -0500

    adding wufoo styles to host on githubpages

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -1,29 +1,39 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var $ = window.jQuery;  // hack, couldn't get browserify to work with a global:jquery and then require('jquery')
+(function e(t, n, r) {
+function s(o, u) {
+    if (!n[o]) {
+        if (!t[o]) {
+            var a = typeof require == "function" && require;if (!u && a)return a(o, !0);if (i)return i(o, !0);var f = new Error("Cannot find module '" + o + "'");throw f.code = "MODULE_NOT_FOUND", f
+        }var l = n[o] = {exports:{}};t[o][0].call(l.exports, function(e) {
+            var n = t[o][1][e];return s(n ? n : e)
+        }, l, l.exports, e, t, n, r)
+    }return n[o].exports
+}var i = typeof require == "function" && require;for (var o = 0; o < r.length; o++)s(r[o]);return s
+})({1:[function(require, module, exports) {
+    var $ = window.jQuery;  // hack, couldn't get browserify to work with a global:jquery and then require('jquery')
 
-// stupid variable height footer. The best solution is to have margin-bottom on body the same as the height of
-// the footer. And then periodically check if the window has resized (debounced of course) and resize.
-var setFooterSize = function() {
+    // stupid variable height footer. The best solution is to have margin-bottom on body the same as the height of
+    // the footer. And then periodically check if the window has resized (debounced of course) and resize.
+    var setFooterSize = function() {
     var footer = $('.footer');
     $('body').css('margin-bottom', footer.height() + 5);
     footer.prev().children().last().css('margin-bottom', '10px');
 };
 
-window.didResize = false;
-setFooterSize();
+    window.didResize = false;
+    setFooterSize();
 
-$(window).resize(function() {
+    $(window).resize(function() {
     window.didResize = true;
 });
 
-setInterval(function() {
+    setInterval(function() {
     if (window.didResize) {
         window.didResize = false;
         setFooterSize();
     }
 }, 400);
 
-// Fix the damn lopsided wufoo name field
-$('#Field2').attr('size','8');
+    // Fix the damn lopsided wufoo name field
+    $('#Field2').attr('size', '8');
 
-},{}]},{},[1]);
+}, {}]}, {}, [1]);
